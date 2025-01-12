eedca1319b8b89c1a20dab1bf5a998cffbc94193
Daniel <cha24074@byui.edu>
2025-01-12 11:02:29 -0700

    finished prep1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        String letter = "";
+        Console.Write("What is your Grade percentage: ");
+        String userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        if (percentage >= 90)
+            {
+            letter = "A";
+            }
+        else if (percentage >= 80)
+            {
+            letter = "B";
+            }
+        else if (percentage >= 70)
+            {
+            letter = "C";
+            }
+        else if (percentage >= 60)
+            {
+            letter = "D";
+            }
+        else
+            {
+            letter = "F";
+            }
+        Console.WriteLine($"Your Grade is {letter}");
+        if (percentage >= 70)
+            {
+            Console.WriteLine("You Passed!!!!!");
+            }
+        else
+            {
+            Console.WriteLine("You Failed this class. You Can do better next time :)");
+            }
+
     }
 }
