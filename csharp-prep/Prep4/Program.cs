9b443798bdcdffd9631a1210a7c3f2935733d601
Daniel <cha24074@byui.edu>
2025-01-16 17:57:29 -0700

    Programmed Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        float avg = 0;
+        int great = 0;
+        float total = 0;
+        List<int> numbers = new List<int>();
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            string usrInput = Console.ReadLine();
+            number = int.Parse(usrInput);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        }
+        foreach (int numb in numbers)
+        {
+            total = total + numb;
+            avg = total / numbers.Count;
+            if (numb > great)
+            {
+                great = numb;
+            }
+        }
+        Console.WriteLine($"Your total: {total}");
+        Console.WriteLine($"The Largest number: {great}");
+        Console.WriteLine($"Your Average is: {avg}");
     }
 }
