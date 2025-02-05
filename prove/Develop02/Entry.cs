336dac1ba73ccf5d99d444c9c5fbd9d9d2b7c7ce
Daniel <cha24074@byui.edu>
2025-02-04 19:38:00 -0700

    added some prompts

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -13,6 +13,14 @@ public class Entry
         prompts.Add("What was the strongest emotion I felt today?");
         prompts.Add("What was the strongest emotion I felt today?");
         prompts.Add("If I had one thing I could do over today, what would it be?");
+        prompts.Add("What was my favorite activity I did today?");
+        prompts.Add("Did I play my favorite game today? What happened during it?");
+        prompts.Add("Have I seen any blessings today?");
+        prompts.Add("What did I eat today was my favorite?");
+        prompts.Add("Did I do anything new today?");
+        prompts.Add("Have I achieved all my goals for today?");
+        prompts.Add("What are my goals for tomorrow?");
+        prompts.Add("Are there any goals from yesterday I can do today?");
     }
     public void GivePrompt()
     {
